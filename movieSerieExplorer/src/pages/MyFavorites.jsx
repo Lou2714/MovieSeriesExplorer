7ffcc8b9727c930ee38ff66da79ac73154fc6941
Lourdes Jiménez <00090220@uca.edu.sv>
2026-03-08 22:23:10 -0600

    :sparkles: Se muestran mis favoritos en su respectiva página

--- movieSerieExplorer/src/pages/MyFavorites.jsx
+++ movieSerieExplorer/src/pages/MyFavorites.jsx
@@ -4,27 +4,28 @@ import { getFavorites } from "../utils/favorites";
 
 import { useEffect, useState } from "react";
 
-
 const MyFavorites = () => {
 
     const [favorites, setFavorites] = useState(null);
 
-    useEffect(() =>{
-
-    },[]);
+    useEffect(() => {
+        showFavorites();
+    },[])
 
     const showFavorites = () => {
-        
+        const myFavorites = getFavorites();
+        setFavorites(myFavorites);
     }
 
     return(
         <div>
             <h1 className="text-center font-bold text-xl text-Wild-Sand-100">Mi lista</h1>
             <div className="flex flex-row flex-wrap gap-y-2 justify-center-safe py-5">
-                <PosterCard />
-                <PosterCard />
-                <PosterCard />
-                <PosterCard />
+                {
+                    favorites?.map((fav) => (
+                        <PosterCard key={fav.id} mediaId={fav.id} mediaType={fav.mediaType} poster={fav.poster_path}/>
+                    ))
+                }
             </div>
         </div>
     )
